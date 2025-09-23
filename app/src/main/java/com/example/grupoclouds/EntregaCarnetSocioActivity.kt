6e3afcdf38e0c94af87c2284e3f08f25fbbc6ff8
Edu Moreno <e.m.morenolp@gmail.com>
2025-09-23 00:59:49 -0300

    delete todo

--- app/src/main/java/com/example/grupoclouds/EntregaCarnetSocioActivity.kt
+++ app/src/main/java/com/example/grupoclouds/EntregaCarnetSocioActivity.kt
@@ -7,7 +7,5 @@ class EntregaCarnetSocioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrega_carnet_socio)
-
-        // TODO: Implement functionality for scanning DNI, displaying member card, user data, and buttons
     }
 }
