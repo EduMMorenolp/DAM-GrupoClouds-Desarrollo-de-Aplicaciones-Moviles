aa0d743f401090956f8e8b10989a8f4bae37fad9
Edu Moreno <e.m.morenolp@gmail.com>
2025-09-23 00:55:33 -0300

    Entrega de carnet

---
+++ app/src/main/java/com/example/grupoclouds/EntregaCarnetSocioActivity.kt
@@ -0,0 +1,13 @@
+package com.example.grupoclouds
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EntregaCarnetSocioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrega_carnet_socio)
+
+        // TODO: Implement functionality for scanning DNI, displaying member card, user data, and buttons
+    }
+}
