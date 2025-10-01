d273ef905db840db2cd868e9125ac1103d14c398
hleopary <cacholeo@gmail.com>
2025-10-01 17:03:14 -0300

    MiembrosActivity agregado no hacer merge sin revisar

---
+++ app/src/main/java/com/example/grupoclouds/Miembro.kt
@@ -0,0 +1,7 @@
+/*package com.example.grupoclouds  // Asegúrate de que el paquete sea el tuyo
+
+data class Miembro(    val nombre: String,
+                       val id: String,
+                       val urlFoto: String // Usaremos una URL para la foto, Glide se encargará de cargarla
+)*/
+
