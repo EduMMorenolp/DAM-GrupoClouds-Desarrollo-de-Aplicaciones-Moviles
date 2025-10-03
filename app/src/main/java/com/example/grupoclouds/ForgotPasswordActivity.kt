5d1203cc35976b964a07938d40438194f00de783
JaviaCode <javiacode@gmail.com>
2025-10-03 15:13:20 -0300

    fix: Icono de cierre en recuperar contraseña
    cambiado el id del boton close en
    activity_forgot_password.xml por btn_close_forgot_password para mantener coherencia con los btnClose de la app

--- app/src/main/java/com/example/grupoclouds/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/grupoclouds/ForgotPasswordActivity.kt
@@ -1,11 +1,19 @@
 package com.example.grupoclouds
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        val btnClose = findViewById<ImageView>(R.id.btn_close_forgot_password)
+        btnClose.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
